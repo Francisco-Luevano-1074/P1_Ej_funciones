065d4ade37bd6c559580e0298921354676bdc295
Francisco Luévano <164532299+Francisco-Luevano-1074@users.noreply.github.com>
2025-02-07 16:47:43 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,21 @@
+// función que no devuelve valor
+// función saludar
+void saludar() {
+  print('Hola, mundo');
+}
+
+void escribeNombre() {
+  print('Francisco Luévano');
+}
+
+void main() {
+  // Francisco Luévano Mat: 22308051281074
+  // Llamar a la función saludar
+  print('Francisco Luévano Mat: 22308051281074');
+  print('Llamando a la función saludar');
+  saludar();
+  print('Llamando a la función escribeNombre');
+  escribeNombre();
+
+}
+
